6d7411aa70baeec02f1f57647f19b16fdb512d56
Palak Totla <totla.1@iitj.ac.in>
2022-01-12 19:20:05 +0530

    -

--- website/views.py
+++ website/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import StickyNotes
+from website.models import StickyNotes
 # Create your views here.
 
 def home(request):
-
     return render(request,'index.html', {})
 
+def highlights(request):
+    highlights= StickyNotes.objects.all()
+    context={
+        'highlights': highlights
+    }
+    return render(request, 'index.html', context)
