801d8e149f57cbe30210ab9da9781fa1c863cc47
Palak Totla <totla.1@iitj.ac.in>
2022-01-12 16:33:17 +0530

    -

--- website/models.py
+++ website/models.py
@@ -1,3 +1,7 @@
+from os import name
 from django.db import models
 
 # Create your models here.
+class StickyNotes(models.Model):
+    note_title = models.CharField(max_length=255)    
+    note_body = models.TextField()  
--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import StickyNotes
 # Create your views here.
 
 def home(request):
+
     return render(request,'index.html', {})
 
