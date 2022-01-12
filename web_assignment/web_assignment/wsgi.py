b281734ca35662a2d6a12f3d2e78028b2aaa6b73
Palak Totla <totla.1@iitj.ac.in>
2022-01-12 12:59:30 +0530

    Update wsgi.py

--- web_assignment/web_assignment/wsgi.py
+++ web_assignment/web_assignment/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_assignment.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_assignment.web_assignment.settings.dev')
 
 application = get_wsgi_application()
